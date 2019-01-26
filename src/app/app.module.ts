4eed1b54008ad039df7d602ec62e0cd60abddf8e
RafaDraw <www.rafadraw@gmail.com>
2019-01-26 16:05:28 -0200

    base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderModule } from './components/template/header/header.module';
+import { MenuModule } from './components/template/menu/menu.module';
+import { FooterModule } from './components/template/footer/footer.module';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    MenuModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
