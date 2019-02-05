cae973f4a6eaa6f56e817d4b879373e5a974fde6
RafaDraw <www.rafadraw@gmail.com>
2019-02-05 18:32:24 -0200

    app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UsersComponent } from './components/users/users.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { AnswerComponent } from './components/answer/answer.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+
+const appRoutes: Routes = [
+  { path: 'newsletter', component: NewsletterComponent },
+  { path: 'answer', component: AnswerComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'socialmedia', component: SocialmediaComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '', component: DashboardComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
