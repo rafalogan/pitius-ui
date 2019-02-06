f31a750042daf6c8cdfb13ffa216830b31708a57
RafaDraw <www.rafadraw@gmail.com>
2019-02-06 17:01:35 -0200

    Refactor routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,8 @@ import { AppComponent } from './app.component';
 import { HeaderModule } from './components/template/header/header.module';
 import { MenuModule } from './components/template/menu/menu.module';
 import { FooterModule } from './components/template/footer/footer.module';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { UsersComponent } from './components/users/users.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { ArticlesComponent } from './components/articles/articles.component';
-import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
-import { NewsletterComponent } from './components/newsletter/newsletter.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { AnswerComponent } from './components/answer/answer.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 
@@ -25,22 +18,14 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   declarations: [
     AppComponent,
     DashboardComponent,
-    UsersComponent,
-    CategoriesComponent,
-    ArticlesComponent,
-    SocialmediaComponent,
-    NewsletterComponent,
-    ContactsComponent,
-    AnswerComponent,
-    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     HeaderModule,
     MenuModule,
     FooterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
