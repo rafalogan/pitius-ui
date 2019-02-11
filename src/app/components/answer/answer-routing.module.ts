eb48b920c91243ecf40506b82647568360e40dd4
RafaDraw <www.rafadraw@gmail.com>
2019-02-11 13:51:02 -0200

    base routes

--- src/app/components/answer/answer-routing.module.ts
+++ src/app/components/answer/answer-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {AnswerComponent} from './answer.component';
+
+const routes: Routes = [
+  { path: '', component: AnswerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
