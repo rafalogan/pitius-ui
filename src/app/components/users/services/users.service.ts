6334f62d63ac387c67b9c303b40c0bca69305c3a
RafaDraw <www.rafadraw@gmail.com>
2019-04-08 16:58:46 -0300

    Service

--- src/app/components/users/services/users.service.ts
+++ src/app/components/users/services/users.service.ts
@@ -11,7 +11,7 @@ import { User } from '../shared/user';
 })
 export class UsersService {
 
-  token: any = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJhZmFlbCBCLiBDYW5kZWlyYSIsImVtYWlsIjoicm9vdEByb290LmNvbSIsImFkbWluIjoxLCJpYXQiOjE1NTExMTAzMzcsImV4cCI6MTU1MTE5NjczN30.Y9K1w_LfhK0gLQJEGOqQ8laYQZh0Sz0OWB3hAj2--5k';
+  token: any = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlJhZmFlbCBCLiBDYW5kZWlyYSIsImVtYWlsIjoicm9vdEByb290LmNvbSIsImFkbWluIjoxLCJpYXQiOjE1NTM1MjM1NjAsImV4cCI6MTU1MzYwOTk2MH0.CpmOwiDOjN7tIver9EgOJ_NfJm97GAeq7EsagjpVlpY';
 
   private readonly API = `${environment.API}users`;
 
